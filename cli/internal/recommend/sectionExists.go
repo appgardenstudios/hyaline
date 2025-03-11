c71d8337cdb57562b68a9edda9a9978833b2f9dd
John Clark <socrates37@gmail.com>
2025-03-11 14:12:12 -0600

    resolve handling no documents

--- cli/internal/recommend/sectionExists.go
+++ cli/internal/recommend/sectionExists.go
@@ -14,6 +14,14 @@ func SectionExists(sectionExists bool, section string, document string, systemID
 	// Note that this should really use a string builder
 	systemPrompt := "You are a senior technical writer who writes clear and accurate system documentation."
 
+	// Create our existence action for use below
+	var existenceAction string
+	if sectionExists {
+		existenceAction = "create"
+	} else {
+		existenceAction = "add content to"
+	}
+
 	// Get documents for the prompt
 	potentialFiles := []string{"package.json", "Makefile"}
 	files, err := sqlite.GetCodeFile(potentialFiles, systemID, current)
@@ -21,6 +29,13 @@ func SectionExists(sectionExists bool, section string, document string, systemID
 		slog.Debug("SectionExists could not get code files", "error", err, "files", files)
 		return "", err
 	}
+
+	// If there are no files, return a generic action
+	if len(*files) < 1 {
+		return fmt.Sprintf(`You should %s the section '%s' in '%s'. The section should describe how to run the project locally, along with any pre-requisites.`, existenceAction, section, document), nil
+	}
+
+	// Format our documents for the prompt
 	formattedDocuments := []string{}
 	for i, doc := range *files {
 		formattedDocuments = append(formattedDocuments, fmt.Sprintf(`  <document index="%d">
@@ -35,7 +50,6 @@ func SectionExists(sectionExists bool, section string, document string, systemID
 </documents>`, strings.Join(formattedDocuments, "\n"))
 
 	// Generate userPrompt
-	// TODO what to do if there are 0 documents?
 	userPrompt := fmt.Sprintf(`%s
 
 Given the documents above, generate documentation describing how to run this %s project locally for development. Be clear, accurate, and show console commands where appropriate. Produce the documentation in the %s format.`, documents, "js", "markdown")
@@ -48,12 +62,6 @@ Given the documents above, generate documentation describing how to run this %s 
 	}
 
 	// Format the action
-	var existenceAction string
-	if sectionExists {
-		existenceAction = "create"
-	} else {
-		existenceAction = "add content to"
-	}
 	action := fmt.Sprintf(`You should %s the section '%s' in '%s'. The contents of that section could contain something like:
 
 %s`, existenceAction, section, document, suggestion)
