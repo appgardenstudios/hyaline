ec639720b9bf95860df023fcaa8ea1bdf6fbf95d
John Clark <socrates37@gmail.com>
2025-03-12 11:13:23 -0600

    basic loading complete

--- cli/internal/code/extractChange.go
+++ cli/internal/code/extractChange.go
@@ -1,18 +1,20 @@
 package code
 
 import (
-	"context"
 	"database/sql"
-	"fmt"
 	"hyaline/internal/config"
 	"hyaline/internal/sqlite"
+	"io"
 	"log/slog"
 	"os"
 	"path/filepath"
+	"slices"
 
 	"github.com/go-git/go-git/v5"
 	"github.com/go-git/go-git/v5/plumbing"
 	"github.com/go-git/go-git/v5/plumbing/object"
+	"github.com/go-git/go-git/v5/utils/merkletrie"
+	"github.com/mattn/go-zglob"
 )
 
 func ExtractChange(system *config.System, head string, base string, db *sql.DB) (err error) {
@@ -42,6 +44,11 @@ func ExtractChange(system *config.System, head string, base string, db *sql.DB) 
 		if !ok {
 			slog.Info("Code Preset Not Found. Skipping...", "system", system, "code", c.ID, "preset", c.Preset)
 			continue
+		}
+		glob, err := zglob.New(preset.Glob)
+		if err != nil {
+			slog.Debug("code.ExtractChange could not instantiate preset glob", "error", err, "glob", preset.Glob)
+			return err
 		}
 		slog.Debug("code.ExtractChange extracting code using preset", "presetID", c.Preset, "preset", preset)
 
@@ -95,18 +102,80 @@ func ExtractChange(system *config.System, head string, base string, db *sql.DB) 
 			slog.Debug("code.ExtractChange could not get base tree", "error", err)
 			return err
 		}
-		diffs, err := object.DiffTreeWithOptions(context.Background(), baseTree, headTree, object.DefaultDiffTreeOptions)
+		// Note that we will eventually want to support renames via object.DiffTreeWithOptions
+		diffs, err := object.DiffTree(baseTree, headTree)
 		if err != nil {
 			slog.Debug("code.ExtractChange could not get diff", "error", err)
 			return err
 		}
 
-		// TODO
+		// Load any files in the diff that match our preset
 		for _, diff := range diffs {
-			fmt.Println(diff.String())
+			slog.Debug("code.ExtractChange processing diff", "diff", diff.String())
+			action, err := diff.Action()
+			if err != nil {
+				slog.Debug("code.ExtractChange could not retrieve action for diff", "error", err, "diff", diff)
+				return err
+			}
+			from, to, err := diff.Files()
+			if err != nil {
+				slog.Debug("code.ExtractChange could not retrieve files for diff", "error", err, "diff", diff)
+				return err
+			}
+			switch action {
+			case merkletrie.Insert:
+				fallthrough
+			case merkletrie.Modify:
+				// TODO make the files match less fragile
+				if glob.Match(to.Name) || slices.Contains(preset.Files, "./"+to.Name) {
+					slog.Debug("code.ExtractChange inserting file", "file", to.Name, "action", action)
+					bytes, err := getBlobBytes(to.Blob)
+					if err != nil {
+						slog.Debug("code.ExtractChange could not retrieve blob from diff", "error", err)
+						return err
+					}
+					err = sqlite.InsertChangeFile(sqlite.ChangeFile{
+						ID:           to.Name,
+						CodeID:       codeId,
+						SystemID:     system.ID,
+						RelativePath: to.Name,
+						Action:       action.String(),
+						RawData:      string(bytes),
+					}, db)
+					if err != nil {
+						slog.Debug("code.ExtractChange could not insert file", "error", err)
+						return err
+					}
+				}
+			case merkletrie.Delete:
+				// TODO make the files match less fragile
+				if glob.Match(from.Name) || slices.Contains(preset.Files, "./"+from.Name) {
+					slog.Debug("code.ExtractChange inserting file", "file", from.Name, "action", action)
+					err = sqlite.InsertChangeFile(sqlite.ChangeFile{
+						ID:           from.Name,
+						CodeID:       codeId,
+						SystemID:     system.ID,
+						RelativePath: from.Name,
+						Action:       action.String(),
+						RawData:      "",
+					}, db)
+					if err != nil {
+						slog.Debug("code.ExtractChange could not insert file", "error", err)
+						return err
+					}
+				}
+			}
 		}
-
 	}
 
+	return
+}
+
+func getBlobBytes(blob object.Blob) (bytes []byte, err error) {
+	r, err := blob.Reader()
+	if err != nil {
+		return
+	}
+	bytes, err = io.ReadAll(r)
 	return
 }
