f9517b414493c32dc87ab77cddabf712b80555e4
John Clark <socrates37@gmail.com>
2025-03-12 08:40:26 -0600

    basic change schema

--- cli/internal/action/extractChange.go
+++ cli/internal/action/extractChange.go
@@ -1,8 +1,13 @@
 package action
 
 import (
+	"database/sql"
+	"errors"
 	"hyaline/internal/config"
+	"hyaline/internal/sqlite"
 	"log/slog"
+	"os"
+	"path/filepath"
 )
 
 type ExtractChangeArgs struct {
@@ -24,17 +29,52 @@ func ExtractChange(args *ExtractChangeArgs) error {
 	))
 
 	// Load Config
-	_, err := config.Load(args.Config)
+	cfg, err := config.Load(args.Config)
 	if err != nil {
 		slog.Debug("ExtractChange could not load the config", "error", err)
 		return err
 	}
 
 	// Create/Scaffold SQLite
-	// TODO
+	absPath, err := filepath.Abs(args.Output)
+	if err != nil {
+		slog.Debug("ExtractChange could not get an absolute path for output", "output", args.Output, "error", err)
+		return err
+	}
+	// Error if file exists as we want to ensure we start from an empty DB
+	// NOTE this is fragile as the file could be created by another program between here and
+	// when we open the DB
+	_, err = os.Stat(absPath)
+	if err == nil {
+		slog.Debug("ExtractChange detected that output db already exists", "absPath", absPath)
+		return errors.New("output file already exists")
+	}
+	db, err := sql.Open("sqlite", absPath)
+	if err != nil {
+		slog.Debug("ExtractChange could not open a new SQLite DB", "dataSourceName", absPath, "error", err)
+		return err
+	}
+	defer db.Close()
+	err = sqlite.CreateChangeSchema(db)
+	if err != nil {
+		slog.Debug("ExtractChange could not create the current schema", "error", err)
+		return err
+	}
 
 	// Get/Insert System
-	// TODO
+	system, err := config.GetSystem(args.System, cfg)
+	if err != nil {
+		slog.Debug("ExtractChange could not locate the system", "system", args.System, "error", err)
+		return err
+	}
+	err = sqlite.InsertChangeSystem(sqlite.ChangeSystem{
+		ID: system.ID,
+	}, db)
+	if err != nil {
+		slog.Debug("ExtractChange could not insert the system", "error", err)
+		return err
+	}
+	slog.Debug("ExtractChange system inserted")
 
 	// Extract/Insert Code
 	// TODO
