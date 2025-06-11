d498bd7428a43e81eb46d92cce0cc1237362e6a3
battle-panda <thuey100+battlepanda@gmail.com>
2025-06-11 10:20:05 -0500

    revert test code

--- cli/scripts/release.sh
+++ cli/scripts/release.sh
@@ -7,14 +7,14 @@ rm -f ./dist/*
 # Build and test hyaline
 make build
 make test
-# make e2e
+make e2e
 
 # Make sure we are on the main branch
-# CURRENT_BRANCH=`git rev-parse --abbrev-ref HEAD`
-# if [ "$CURRENT_BRANCH" != "main" ]; then
-#   echo "Must be on main branch to release."
-#   exit 1
-# fi
+CURRENT_BRANCH=`git rev-parse --abbrev-ref HEAD`
+if [ "$CURRENT_BRANCH" != "main" ]; then
+  echo "Must be on main branch to release."
+  exit 1
+fi
 
 # Calculate tag YYYY-MM-DD-HASH
 DATE=`git log -n1 --pretty='%cd' --date=format:'%Y-%m-%d'`
