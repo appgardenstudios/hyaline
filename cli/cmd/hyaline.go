e795c78babdc70b4127af4787d9caeea0c69456b
John Clark <socrates37@gmail.com>
2025-07-03 07:30:46 -0600

    grammar

--- cli/cmd/hyaline.go
+++ cli/cmd/hyaline.go
@@ -14,7 +14,7 @@ var Version = "unknown"
 
 var usage = "Maintain Your Documentation - Find, Fix, and Prevent Documentation Issues."
 
-var licenseNote = "Note: This software requires a license for any purpose other than evaluation or demonstration. By using this software you attest that you are either 1) using Hyaline for evaluation or demonstration purposes or 2) that you have a current and valid license to use Hyaline."
+var licenseNote = "Note: This software requires a license for any purpose other than evaluation or demonstration. By using this software you attest that you are either 1) using Hyaline for evaluation or demonstration purposes or 2) you have a current and valid license to use Hyaline."
 
 func main() {
 	// Configure logger
--- cli/scripts/assets/release-notes.md
+++ cli/scripts/assets/release-notes.md
@@ -1,2 +1,2 @@
 ## Note:
-This software requires a license for any purpose other than evaluation or demonstration. By downloading this software you attest that you are either 1) using Hyaline for evaluation or demonstration purposes or 2) that you have a current and valid license to use Hyaline.
+This software requires a license for any purpose other than evaluation or demonstration. By downloading this software you attest that you are either 1) using Hyaline for evaluation or demonstration purposes or 2) you have a current and valid license to use Hyaline.
