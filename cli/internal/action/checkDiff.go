00785c35aab3f6fc1b1bad1e8b99c9adb3621c8a
John Clark <socrates37@gmail.com>
2025-07-25 10:19:27 -0600

    get diff

--- cli/internal/action/checkDiff.go
+++ cli/internal/action/checkDiff.go
@@ -2,10 +2,8 @@ package action
 
 import (
 	"errors"
+	"hyaline/internal/code"
 	"hyaline/internal/config"
-	"hyaline/internal/docs"
-	"hyaline/internal/github"
-	"hyaline/internal/sqlite"
 	"log/slog"
 )
 
@@ -49,49 +47,57 @@ func CheckDiff(args *CheckDiffArgs) error {
 		return err
 	}
 
-	// Get Pull Request
-	var pr *string
-	if args.PullRequest != "" {
-		if cfg.GitHub.Token == "" {
-			return errors.New("github token required to retrieve pull-request information")
-		}
-		pr, err = github.GetPullRequest(args.PullRequest, cfg.GitHub.Token)
-		if err != nil {
-			slog.Debug("action.CheckDiff could not get pull request", "pull-request", args.PullRequest, "error", err)
-			return err
-		}
-		slog.Debug("action.CheckDiff retrieved pull-request", "pull-request", *pr) // TODO clean up
-	}
+	// // Get Pull Request
+	// var pr *string
+	// if args.PullRequest != "" {
+	// 	if cfg.GitHub.Token == "" {
+	// 		return errors.New("github token required to retrieve pull-request information")
+	// 	}
+	// 	pr, err = github.GetPullRequest(args.PullRequest, cfg.GitHub.Token)
+	// 	if err != nil {
+	// 		slog.Debug("action.CheckDiff could not get pull request", "pull-request", args.PullRequest, "error", err)
+	// 		return err
+	// 	}
+	// 	slog.Debug("action.CheckDiff retrieved pull-request", "pull-request", *pr) // TODO clean up
+	// }
 
-	// Get Issue(s)
-	issues := []*string{}
-	if len(args.Issues) > 0 {
-		if cfg.GitHub.Token == "" {
-			return errors.New("github token required to retrieve issue information")
-		}
-		for _, issue := range args.Issues {
-			body, err := github.GetIssue(issue, cfg.GitHub.Token)
-			if err != nil {
-				slog.Debug("action.CheckDiff could not get issue", "issue", issue, "error", err)
-				return err
-			}
-			issues = append(issues, body)
-		}
-		slog.Debug("action.CheckDiff retrieved issues", "issues", issues) // TODO clean up
-	}
+	// // Get Issue(s)
+	// issues := []*string{}
+	// if len(args.Issues) > 0 {
+	// 	if cfg.GitHub.Token == "" {
+	// 		return errors.New("github token required to retrieve issue information")
+	// 	}
+	// 	for _, issue := range args.Issues {
+	// 		body, err := github.GetIssue(issue, cfg.GitHub.Token)
+	// 		if err != nil {
+	// 			slog.Debug("action.CheckDiff could not get issue", "issue", issue, "error", err)
+	// 			return err
+	// 		}
+	// 		issues = append(issues, body)
+	// 	}
+	// 	slog.Debug("action.CheckDiff retrieved issues", "issues", issues) // TODO clean up
+	// }
 
-	// Get Documents
-	docDB, err := sqlite.InitInput(args.Documentation)
+	// // Get Documents
+	// docDB, err := sqlite.InitInput(args.Documentation)
+	// if err != nil {
+	// 	slog.Debug("action.CheckDiff could not initialize documentation db", "documentation", args.Documentation, "error", err)
+	// 	return err
+	// }
+	// documents, err := docs.GetFilteredDocs(&cfg.Check.Documentation, docDB)
+	// if err != nil {
+	// 	slog.Debug("action.CheckDiff could not get filtered documents", "error", err)
+	// 	return err
+	// }
+	// slog.Debug("action.CheckDiff retrieved documents", "documents", documents) // TODO clean up
+
+	// Get Diff
+	files, err := code.GetFilteredDiff(args.Path, args.Head, args.HeadRef, args.Base, args.BaseRef, &cfg.Check.Code)
 	if err != nil {
-		slog.Debug("action.CheckDiff could not initialize documentation db", "documentation", args.Documentation, "error", err)
+		slog.Debug("action.CheckDiff could not get filtered diff", "error", err)
 		return err
 	}
-	documents, err := docs.GetFilteredDocs(&cfg.Check.Documentation, docDB)
-	if err != nil {
-		slog.Debug("action.CheckDiff could not get filtered documents", "error", err)
-		return err
-	}
-	slog.Debug("action.CheckDiff retrieved documents", "documents", documents) // TODO clean up
+	slog.Debug("action.CheckDiff retrieved files from diff", "files", files) // TODO clean up
 
 	return nil
 }
