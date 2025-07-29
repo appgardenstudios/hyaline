25c54f48f101c5b9e87741ee5c35847fc6690723
John Clark <socrates37@gmail.com>
2025-07-29 08:39:58 -0600

    add e2e tests

---
+++ cli/e2e/checkDiff_test.go
@@ -0,0 +1,36 @@
+package e2e
+
+import (
+	"fmt"
+	"testing"
+	"time"
+)
+
+func TestTestCheckDiff(t *testing.T) {
+	goldenPath := "./_golden/check-diff.json"
+	outputPath := fmt.Sprintf("./_output/check-diff-%d.json", time.Now().UnixMilli())
+	args := []string{
+		"check", "diff",
+		"--config", "./_input/check-diff/hyaline.yml",
+		"--documentation", "./_input/check-diff/documentation.sqlite",
+		"--path", "../../../hyaline-example",
+		"--base", "main",
+		"--head", "feat-1",
+		"--pull-request", "appgardenstudios/hyaline-example/1",
+		"--issue", "appgardenstudios/hyaline-example/2",
+		"--issue", "appgardenstudios/hyaline-example/3",
+		"--output", outputPath,
+	}
+
+	stdOutStdErr, err := runBinary(args, t)
+	t.Log(string(stdOutStdErr))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if true || *update {
+		updateGolden(goldenPath, outputPath, t)
+	}
+
+	compareFiles(goldenPath, outputPath, t)
+}
